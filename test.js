0735db97a8255f0f7018eb61e7d5fd525f65d1c8
juan jose barrera <juanjoseb9805@gmail.com>
2026-02-02 12:42:36 -0500

    feat: searching products by name on real time

--- test.js
+++ test.js
@@ -1,5 +1,7 @@
 import * as Api from "./src/services/api.js";
 
+const container = document.querySelector("#container");
+
 // const regModal = document.querySelector("#regModal");
 // const editModal = document.querySelector("#editModal");
 const registerForm = document.querySelector("#registerForm");
@@ -35,4 +37,51 @@ editForm.addEventListener("submit", async function () {
     email: editModalEmail.value,
     password: editModalPass.value,
   });
+});
+const renderProducts = function (products) {
+  container.innerHTML = "";
+
+  products.forEach((product) => {
+    const html = `
+      <div class="col-4">
+        <div class="card">
+          <img class="card-img-top" src="${product.image}" alt="" />
+          <div class="card-body">
+            <div class="d-flex justify-content-between">
+              <h4 class="card-title">${product.name}</h4>
+              <p class="card-text text-success fw-bolder">${product.price}</p>
+            </div>
+            <p class="card-text">${product.description}</p>
+            <button data-id="${product.id}" class="btn btn-success">
+              Add to order
+            </button>
+            <button data-id="${product.id}" class="btn btn-danger">
+              Delete
+            </button>
+          </div>
+        </div>
+      </div>
+    `;
+
+    container.insertAdjacentHTML("beforeend", html);
+  });
+};
+
+let allProducts = [];
+
+window.addEventListener("DOMContentLoaded", async function () {
+  allProducts = await Api.getData("products");
+  renderProducts(allProducts);
+});
+
+const searchInput = document.querySelector("#searchInput");
+
+searchInput.addEventListener("input", function (e) {
+  const query = e.target.value.toLowerCase().trim();
+
+  const filteredProducts = allProducts.filter((product) =>
+    product.name.toLowerCase().includes(query),
+  );
+
+  renderProducts(filteredProducts);
 });
